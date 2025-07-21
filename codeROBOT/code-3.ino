6d454400ea9f421c66a9a753b1e7679c599a25b0
Valera-2321 <96603687+Valera-2321@users.noreply.github.com>
2025-07-21 17:08:11 -0500

    codigo implementando el uso de seguidpor de linea si no se lograce a cumplir el otro preoceso de automatizacion por mapeo con los sensores ENCODERs

--- codeROBOT/code-3.ino
+++ codeROBOT/code-3.ino
@@ -1,90 +1,78 @@
-// Definir pines
-const int sensorPins[] = {2, 3}; // Pines de los sensores
-const int motorPins [] [2] = {{4, 5), (6, 7}}; // Pines de los motores
-const int enablePins[] = {9, 10); // Pines de habilitación de los motores
+const int sensorPins[] = {2, 3};  
+const int motorPins[2][2] = {{4, 5}, {6, 7}};  
+const int enablePins[] = {9, 10};
+const int MOTOR_SPEED = 90;     // <--- VELOCIDAD SEGURA
+const int TURN_SPEED = 70;      // <--- VELOCIDAD PARA GIRO
 
-void setup() {
-  // Configurar pines de los sensores
+void setup() { 
   for (int i = 0; i < 2; i++) {
-    pinMode (sensorPins [i], INPUT);
+    pinMode(sensorPins[i], INPUT);
   }
-
-
-  // Configurar pines del motor
   for (int i = 0; i < 2; i++) {
-    pinMode (motorPins [i] [0], OUTPUT);
-    pinMode (motorPins [i] [1], OUTPUT);
-    pinMode (enablePins[i], OUTPUT);
-  }
-  // Inicializar la comunicación serial
+    pinMode(motorPins[i][0], OUTPUT);
+    pinMode(motorPins[i][1], OUTPUT);
+    pinMode(enablePins[i], OUTPUT);
+  } 
   Serial.begin(9600);
 }
 
-void loop() {
-  // Leer los valores de los sensores
+void loop() { 
   int sensorValues[2];
   for (int i = 0; i < 2; i++) {
     sensorValues[i] = digitalRead(sensorPins[i]);
   }
-
-  // Mostrar los valores de los sensores en el monitor serial
+ 
   Serial.print("Left Sensor: ");
   Serial.print(sensorValues[0]);
   Serial.print(" | Right Sensor: ");
   Serial.println(sensorValues[1]);
-
-  // Controlar el motor basado en los valores de los sensores == HIGH) {
+ 
   if (sensorValues[0] == HIGH && sensorValues[1] == HIGH) {
-    // Ambos sensores detectan línea, avanzar recto move Forward();
+    moveForward();
   } else if (sensorValues[0] == LOW && sensorValues[1] == HIGH) {
-    // Solo el sensor derecho detecta línea, girar a la izquierda turnLeft();
-  } else if (sensorValues[0] == HIGH && sensorValues [1] == LOW) {
-    // Solo el sensor izquierdo detecta línea, girar a la derecha 
+    turnLeft();
+  } else if (sensorValues[0] == HIGH && sensorValues[1] == LOW) {
     turnRight();
   } else {
-    // Ninguno de los sensores detecta línea, detenerse 
     stopMotors();
   }
-
-  // Esperar un poco antes de la siguiente lectura 
+ 
   delay(100);
 }
 
 void moveForward() {
-}
-for (int i = 0 ; i < 2 ; i++) {
-    analogWrite (enablePins[i], 255);
-  }
-  digitalWrite(motorPins [0] [0], HIGH);
-  digitalWrite (motorPins [0] [1], LOW);
-  digitalWrite(motorPins [1] [0], HIGH);
-  digitalWrite (motorPins [1] [1], LOW);
+  analogWrite(enablePins[0], MOTOR_SPEED);
+  analogWrite(enablePins[1], MOTOR_SPEED);
+  digitalWrite(motorPins[0][0], HIGH);
+  digitalWrite(motorPins[0][1], LOW);
+  digitalWrite(motorPins[1][0], HIGH);
+  digitalWrite(motorPins[1][1], LOW);
 }
 
 void turnLeft() {
-  analogWrite (enablePins[0], 255);
-  analogWrite (enablePins [1], 255);
-  digitalWrite (motorPins [0] [0], LOW);
-  digitalWrite (motorPins [0] [1], HIGH);
-  digitalWrite(motorPins [1] [0], HIGH);
-  digitalWrite (motorPins [1] [1], LOW);
+  analogWrite(enablePins[0], TURN_SPEED);
+  analogWrite(enablePins[1], TURN_SPEED);
+  digitalWrite(motorPins[0][0], LOW);   // Izq atrás
+  digitalWrite(motorPins[0][1], HIGH);
+  digitalWrite(motorPins[1][0], HIGH);  // Der adelante
+  digitalWrite(motorPins[1][1], LOW);
 }
 
 void turnRight() {
-  analogWrite (enablePins[0], 255);
-  analogWrite (enablePins [1], 255);
-  digitalWrite(motorPins [0] [0], HIGH);
-  digitalWrite(motorPins [0] [1], LOW);
-  digitalWrite(motorPins [1] [0], LOW);
-  digitalWrite (motorPins [1] [1], HIGH);
+  analogWrite(enablePins[0], TURN_SPEED);
+  analogWrite(enablePins[1], TURN_SPEED);
+  digitalWrite(motorPins[0][0], HIGH);  // Izq adelante
+  digitalWrite(motorPins[0][1], LOW);
+  digitalWrite(motorPins[1][0], LOW);   // Der atrás
+  digitalWrite(motorPins[1][1], HIGH);
 }
 
 void stopMotors() {
-  for (int i = 0 ; i < 2 i++) { 
+  for (int i = 0 ; i < 2 ; i++) { 
     analogWrite(enablePins[i], 0);
   }
-  for (int i = 0 ; i < 2 i++) {
-    digitalWrite(motorPins [i] [0], LOW);
-    digitalWrite (motorPins [i] [1], LOW);
+  for (int i = 0 ; i < 2 ; i++) {
+    digitalWrite(motorPins[i][0], LOW);
+    digitalWrite(motorPins[i][1], LOW);
   }
 }
